9b1fc2bad1a818f3072165b0951a7892f7ccf5d7
Enrique Pinula <260@gruporeal.ad>
2024-09-19 08:51:41 -0600

    Corrección de obtención de datos
    Se cambia la obtención de detalles de pokemons para que sea realizada desde su componente directo

--- frontend/src/app/shared-components/seccion-pokemon.component.ts
+++ frontend/src/app/shared-components/seccion-pokemon.component.ts
@@ -6,12 +6,12 @@ import { Component, Input } from '@angular/core';
   imports: [],
   template: `
     <div class="mx-auto max-w-2xl lg:max-w-7xl">
-      <p class="mt-6 text-lg font-bold leading-8 text-gray-900 sm:text-2xl">{{tittle}}</p>
+      <p class="mt-6 text-lg font-bold leading-8 text-gray-900 sm:text-2xl">{{title}}</p>
       <ng-content></ng-content>
     </div>
   `,
   styles: ``
 })
 export class SeccionPokemonComponent {
-  @Input({ required: true }) tittle!: string;
+  @Input({ required: true }) title!: string;
 }
